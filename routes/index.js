92ae5dc15bce1d35a98e2972407c8e77f1aec7da
Andrew Smith <andrewsmith@alumni.stanford.edu>
2020-10-07 09:49:19 -0700

    Add a countdown.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const launchTime = new Date('12/29/2020 10:00').getTime()
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const currentTime = new Date().getTime()
+  const numberOfMilliseconds = parseInt(launchTime - currentTime)
+  res.render(
+    'index', 
+    { title: 'My Awesome Game', countDown: numberOfMilliseconds }
+  );
 });
 
 module.exports = router;
